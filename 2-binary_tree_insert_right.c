d83d9fecdb5d734bcdd3aaae35ae4b92a0499076
=Nii <=nii4tettey@gmail.com>
2022-08-19 04:59:32 +0200

    Inserting to right node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
- * @parent: pointer to the node to insert the left-child in
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
  * @value: value to store in the new node
  *
  * Description: If parent already has a left-child, the new node must take its
@@ -22,5 +22,5 @@ right_child->right = parent->right;
 if (right_child->right != NULL)
 right_child->right->parent = right_child;
 parent->right = right_child;
-return(right_child);
+return (right_child);
 }
